0e62d72a7fbeea03ee6a240df00d6c95b67287bf
janejane012 <janelibraus@gmail.com>
2019-01-17 14:48:48 +0700

    ..

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import "../Styles/bootstrap/bootstrap.min.css";
 import styled from "styled-components";
 
-// const BgImg = styled.img`
-// width: '88%';
-// `
-const bgImage = require('../Pictures/bgLogin.png');
+ const LogoImg = styled.img`
+ width: '88%';
+ `
+const bgImage = require('../Pictures/bg.png');
 const bgStyle = {
-  width: '100%',
+  width: '1368px',
   height: '912px',
   backgroundImage: `url(${bgImage})`,
   backgroundSize: 'cover'
@@ -16,16 +16,25 @@ const bgStyle = {
 class LoginView extends Component {
   render() {
     return(
+      //bg
       <div className="bgComponent" style={bgStyle} >
-        <h1 align="center">....</h1>
+        <h1 align="center">.....</h1>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <LogoImg src={require("../Pictures/logo.png")} alt=""/>
+            </div>
+            <div class="col-sm">
+              <div class="row">topic</div>
+              <div class="row">username</div>
+              <div class="row">password</div>
+              <div class="row">button</div>
+            </div>
+          </div>
+        </div>
+
       </div>
-      // <div>
-      // <BgImg src={require("../Pictures/bg.png")} alt=""/>
-      // </div>
     );
-
-
-    //<div>Login Call</div>;
   }
 }
 
