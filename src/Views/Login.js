d2a7ed618d9bc9fd66e8d0ed5fa3ca3696f2a429
janejane012 <janelibraus@gmail.com>
2019-01-17 13:27:25 +0700

    bg for user lohin page

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import "../Styles/bootstrap/bootstrap.min.css";
+import styled from "styled-components";
+
+// const BgImg = styled.img`
+// width: '88%';
+// `
+const bgImage = require('../Pictures/bgLogin.png');
+const bgStyle = {
+  width: '100%',
+  height: '800px',
+  backgroundImage: `url(${bgImage})`,
+  backgroundSize: 'cover'
+};
 
 class LoginView extends Component {
   render() {
-    return <div>Login Call</div>;
+    return(
+      <div className="bgComponent" style={bgStyle} >
+        <h1 align="center">....</h1>
+      </div>
+      // <div>
+      // <BgImg src={require("../Pictures/bg.png")} alt=""/>
+      // </div>
+    );
+
+
+    //<div>Login Call</div>;
   }
 }
 
