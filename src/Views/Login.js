e316228a4c4c4aecb4ee0ce935b0a22716e0e437
tulathorn <tul.tulathorn@gmail.com>
2019-01-17 13:33:57 +0700

    [COMMIT FOR PULLING]

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
-import "../Styles/bootstrap/bootstrap.min.css";
-import styled from "styled-components";
+import React, { Component } from 'react'
+import '../Styles/bootstrap/bootstrap.min.css'
+import styled from 'styled-components'
 
 // const BgImg = styled.img`
 // width: '88%';
 // `
-const bgImage = require('../Pictures/bgLogin.png');
+const bgImage = require('../Pictures/bgLogin.png')
 const bgStyle = {
-  width: '100%',
-  height: '800px',
   backgroundImage: `url(${bgImage})`,
-  backgroundSize: 'cover'
-};
+  backgroundSize: 'absolute'
+}
 
 class LoginView extends Component {
   render() {
-    return(
-      <div className="bgComponent" style={bgStyle} >
+    return (
+      <div className="bgComponent" style={bgStyle}>
         <h1 align="center">....</h1>
       </div>
       // <div>
       // <BgImg src={require("../Pictures/bg.png")} alt=""/>
       // </div>
-    );
-
+    )
 
     //<div>Login Call</div>;
   }
 }
 
-export default LoginView;
+export default LoginView
