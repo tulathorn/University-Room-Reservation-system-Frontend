de3472e4d19529dcaa38a8d11377b1de570f9401
janejane012 <janelibraus@gmail.com>
2019-01-20 16:38:06 +0700

    main/search page

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import Navbar from '../Components/Navbar'
 
+const textColor = {
+  color: 'white'
+}
 const Title = styled.h1`
   color: #1F384B;
 `
+const Title_1 = styled.h2`
+  color: white;
+`
+const Title_2 = styled.h4`
+  color: white;
+`
+const jumbotronImage = require('../Pictures/bg.png');
+const jumbotronStyle = {
+  width: 'auto',
+  height: 'auto',
+  backgroundImage: `url(${jumbotronImage})`,
+  backgroundSize: 'absolute'
+}
+
 
 class MainView extends Component {
   render() {
@@ -12,34 +29,130 @@ class MainView extends Component {
       <div>
         <Navbar />
         <div className="container">
-          <br/><center><Title>Welcome to University Room Reservation System</Title></center>
+          <br/><center><Title>Welcome to University Room Reservation System</Title></center><br/>
 
-            <div class="row">
-              <div class="jumbotron">
-                <h2>Find a Room</h2>
 
-                <div class="row"></div>
-                <div class="row"></div>
-                <div class="row">
-                  <h4>Filters :</h4><br/>
-                  <div class="row">
-                    <div class="col-sm">
-                      One of three columns
+
+               <div class="jumbotron" style={jumbotronStyle}>
+                  <Title_1>Find a Room</Title_1>
+
+                  <hr class="my-4"/>
+                  <Title_2>Info :</Title_2>
+                  <form>
+                    <div class="form-row">
+                      <div class="form-group col-md-4">
+                        <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>Date</label>
+                        <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                          <option selected>Choose...</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>Building</label>
+                        <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                          <option selected>Choose...</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-md-4">
+                        <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>Size</label>
+                        <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                          <option selected>Choose...</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
                     </div>
-                    <div class="col-sm">
-                      One of three columns
+                  </form>
+
+                <hr class="my-4"/>
+                <Title_2>Time :</Title_2>
+                  <form class="form-inline">
+                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>From</label>
+                    <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>:</label>
+                    <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>To</label>
+                    <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>:</label>
+                    <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </form>
+
+                <hr class="my-4"/>
+                <Title_2>Filters :</Title_2>
+                  <div class="form-row">
+
+                    <div class="col-sm-4">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <label class="form-check-label" for="defaultCheck1" style={textColor}>
+                          Default checkbox
+                        </label><br/>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <label class="form-check-label" for="defaultCheck1" style={textColor}>
+                          Default checkbox
+                        </label>
+                      </div>
                     </div>
-                    <div class="col-sm">
-                      One of three columns
+
+                    <div class="col-sm-4">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <label class="form-check-label" for="defaultCheck1" style={textColor}>
+                          Default checkbox
+                        </label><br/>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <label class="form-check-label" for="defaultCheck1" style={textColor}>
+                          Default checkbox
+                        </label>
+                      </div>
                     </div>
+
+                    <div class="col-sm-4">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <label class="form-check-label" for="defaultCheck1" style={textColor}>
+                          Default checkbox
+                        </label><br/>
+                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                        <label class="form-check-label" for="defaultCheck1" style={textColor}>
+                          Default checkbox
+                        </label>
+                      </div>
+                    </div>
+
                   </div>
                 </div>
 
-              </div>
-            </div>
-            <div class="row">
+
+
               <center><button type="button" class="btn btn-outline-dark">Search</button></center>
-            </div>
+
         </div>
       </div>
     )
