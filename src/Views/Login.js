1f3bfbe54495902f92aded368b7ff4c805886fa7
janejane012 <janelibraus@gmail.com>
2019-01-21 00:00:50 +0700

    login

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -2,20 +2,17 @@ import React, { Component } from 'react'
 import '../Styles/bootstrap/bootstrap.min.css'
 import styled, { createGlobalStyle } from 'styled-components'
 
- const LogoImg = styled.img`
- width: '88%';
- `
-const bgImage = require('../Pictures/bg.png');
-const bgStyle = {
-  width: '1368px',
-  height: '912px',
-  backgroundImage: `url(${bgImage})`,
-  backgroundSize: 'absolute'
-}
+ const logoImage = require('../Pictures/logoBg.png');
+ const logoStyle = {
+   width: 'auto',
+   height: 'auto',
+   backgroundImage: `url(${logoImage})`,
+   backgroundSize: 'absolute'
+ }
 
-const bgColor = {
+const cardColor = {
   backgroundColor: 'white'
-};
+}
 
 const text1Style = {
   color: 'white',
@@ -27,61 +24,88 @@ const text2Style = {
   textAlign: 'center'
 };
 
+const bgColor = {
+  backgroundColor: '#1F384B'
+}
+
+const languageLink = {
+  color: 'white'
+}
+
+const helpLink = {
+  color: '#1F384B'
+}
+
 class LoginView extends Component {
   render() {
     return(
 
-      <div className="bgComponent" style={bgStyle} >
+      <div style={bgColor} >
 
         <div class="container">
-          <div class="row justify-content-center">
+
+          <div class="row">
             <div class="col-sm" >
               <p style={text1Style}>Admin</p>
             </div>
           </div>
+
           <div class="row justify-content-center">
-            <div class="col-sm-auto" >
-              <LogoImg src={require("../Pictures/logoBig.png")} alt=""/>
+
+            <div class="col-sm-4" style={logoStyle}>
+              <br/><br/>
+              <center>
+                <img src={require("../Pictures/logoText.png")} alt=""/>
+              </center>
+              <br/><br/>
             </div>
 
 
-              <div style={bgColor} class="col-sm" >
-                <div class="row"><h1 align="center">Please enter KMUTT username and password. Click on the Login button to continue.</h1></div>
+            <div style={cardColor} class="col-sm-8" >
+              <br/>
+              <div class="jumbotron">
 
-                <div class="row">
+                <h3 align="left">
+                  Please enter KMUTT username and password. Click on the Login button to continue.
+                </h3>
+
+                <br/>
+
+                <form>
                   <div class="form-group">
-                    <label for="exampleFormControlInput1">Username</label>
-                    <input type="username" class="form-control" id="usernameInput" placeholder="username"/>
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                   </div>
-                </div>
 
-                <div class="row">
                   <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
                   </div>
-                </div>
+                </form>
 
                 <div class="row">
-
-
-                  <div class="row">
-                    <div class="col-sm"><button type="submit" class="btn btn-primary">Submit</button></div>
-                    <div class="col-sm"><p><u>Help</u></p></div>
+                  <div class="col-sm">
+                    <button type="submit" class="btn btn-primary">Submit</button>
                   </div>
-
+                  <div class="col-sm">
+                    <button type="button" class="btn btn-link" style={helpLink}><u>Help</u></button>
+                  </div>
                 </div>
+
+
 
 
               </div>
-
-          </div>
-
-          <div class="row justify-content-center">
-            <div class="col-sm" >
-              <br/><center><p style={text2Style}>English Language / ภาษาไทย</p></center>
             </div>
+
           </div>
+
+          <center>
+            <button type="button" class="btn btn-link" style={languageLink}>English</button>
+            <button type="button" class="btn btn-link" style={languageLink}>ภาษาไทย</button>
+            <br/><br/><br/>
+          </center>
 
         </div>
 
