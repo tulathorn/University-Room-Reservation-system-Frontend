d2fd9cfa51ab602a5b1c0ef4109b821cf88c849c
janejane012 <janelibraus@gmail.com>
2019-01-20 15:18:12 +0700

    main

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -5,6 +5,7 @@ import Navbar from '../Components/Navbar'
 const Title = styled.h1`
   color: #1F384B;
 `
+
 class MainView extends Component {
   render() {
     return (
@@ -12,8 +13,33 @@ class MainView extends Component {
         <Navbar />
         <div className="container">
           <br/><center><Title>Welcome to University Room Reservation System</Title></center>
-          <div >
 
+            <div class="row">
+              <div class="jumbotron">
+                <h2>Find a Room</h2>
+
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row">
+                  <h4>Filters :</h4><br/>
+                  <div class="row">
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                    <div class="col-sm">
+                      One of three columns
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <div class="row">
+              <center><button type="button" class="btn btn-outline-dark">Search</button></center>
+            </div>
         </div>
       </div>
     )
