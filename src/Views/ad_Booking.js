69d096d424cba050074cc22f7a80ad5c542f1f90
janejane012 <janelibraus@gmail.com>
2019-01-28 14:33:53 +0700

    l

--- src/Views/ad_Booking.js
+++ src/Views/ad_Booking.js
@@ -11,11 +11,97 @@ class AdBookingView extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <div className="container">
-          <form class="form-inline">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+
+          <div class="row"><h2>CPE1121</h2></div>
+          <div class="row">
+            <div class="col-6">
+              <div class="row">
+                <center><img src={require("../Pictures/testclassroom.jpg")} width="500" alt=""/></center>
+              </div>
+              <br/>
+              <div class="row">
+                <div class="col-3">
+                  <p><b>Information : </b></p>
+                </div>
+                <div class="col">
+                  <p>People Capacity : 50</p>
+                  <p>Building : Witsawa Watthana</p>
+                  <p>Floor : 11</p>
+                  <p>Room number : CPE1121</p>
+                </div>
+              </div>
+              <div class="row">
+
+                <div class="col-3">
+                  <p><b>Amenity : </b></p>
+                </div>
+                <div class="col">
+
+                </div>
+
+
+              </div>
+
+
+            </div>
+            <div class="col-1">
+
+            </div>
+            <div class="col-5">
+              <form action="">
+                      <div className="form-group">
+                        <label for="info1">Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="info1"
+                          placeholder="Firstname"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="info2">Surname</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="info2"
+                          placeholder="Lastname"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="info3">Email Address</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="info3"
+                          placeholder="Email Address"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="info4">Phone Number</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="info4"
+                          placeholder="Phone Number"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label for="info5">Purpose</label>
+                        <textarea
+                          className="form-control"
+                          id="info5"
+                          placeholder="Identify your purpose for booking this room..."
+                          rows="5"
+                        />
+                      </div>
+                      <button type="submit" className="btn btn-primary">
+                        Submit
+                      </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     )
