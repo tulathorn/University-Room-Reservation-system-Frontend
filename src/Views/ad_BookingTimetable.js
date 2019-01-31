699c9850555c7d21cfce78cf8c73e349953141a9
Arnan Hirunratanakorn <honhon015@hotmail.com>
2019-01-31 23:28:29 +0700

    Timetable

--- src/Views/ad_BookingTimetable.js
+++ src/Views/ad_BookingTimetable.js
@@ -13,10 +13,78 @@ class AdBookingTimatableView extends Component {
       <div>
         <AdNavbar />
         <div className="container">
-          <form class="form-inline">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+          <div class="row"><h2>Booking Timetable</h2></div><br/>
+          <div class="row">
+            <div class="col-5">
+              <div class="row">
+                <div class="col-2">
+                  <button class="btn btn-info btn-lg btn-block" disabled></button>
+                </div>
+                <div class="col-10">
+                  <p>Available Time</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-2">
+                  <button class="btn btn-danger btn-lg btn-block" disabled></button>
+                </div>
+                <div class="col-10">
+                  <p>Reserved Time</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-2">
+                  <button class="btn btn-dark btn-lg btn-block" disabled></button>
+                </div>
+                <div class="col-10">
+                  <p>Unavailable Time</p>
+                </div>
+              </div>
+              
+              <div class="row">
+                <button type="button" class="btn btn-outline-dark">Back</button>
+              </div>
+            </div>
+            <div class="col-7">
+              <div class="row">
+                <div class="col-1">
+                  <p>0</p>
+                </div>
+                <div class="col-11">
+                  <button class="btn btn-dark btn-lg btn-block" disabled>-</button>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-1">
+                  <p>1</p>
+                </div>
+                <div class="col-11">
+                  <button class="btn btn-danger btn-lg btn-block" disabled>58070503412 : YWC Meeting</button>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-1">
+                  <p>2</p>
+                </div>
+                <div class="col-11">
+                  <button class="btn btn-danger btn-lg btn-block" disabled>-</button>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-1">
+                  <p>3</p>
+                </div>
+                <div class="col-11">
+                  <button class="btn btn-info btn-lg btn-block" disabled>-</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
