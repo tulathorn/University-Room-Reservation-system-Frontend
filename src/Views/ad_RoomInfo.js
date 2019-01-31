55179b595f7d61a0e7d38fc23ba3b42e13ba5532
Arnan Hirunratanakorn <honhon015@hotmail.com>
2019-01-31 22:50:17 +0700

    update room info

--- src/Views/ad_RoomInfo.js
+++ src/Views/ad_RoomInfo.js
@@ -76,61 +76,24 @@ class AdRoomInfoView extends Component {
             
             <div class="col-6">
               <div className="jumbotron text-white" style={jumbotronStyle}>
-                <form action="">
-                  <div className="form-group">
-                    <label for="info1">Name</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="info1"
-                      placeholder="Firstname"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label for="info2">Surname</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="info2"
-                      placeholder="Lastname"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label for="info3">Email Address</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="info3"
-                      placeholder="Email Address"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label for="info4">Phone Number</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="info4"
-                      placeholder="Phone Number"
-                      />
-                  </div>
-                  <div className="form-group">
-                    <label for="info5">Purpose</label>
-                    <textarea
-                      className="form-control"
-                      id="info5"
-                      placeholder="Identify your purpose for booking this room..."
-                      rows="5"
-                    />
-                  </div>
-                  <button type="submit" className="btn btn-primary">
-                    Submit
-                  </button>
-                </form>
+                <h3>Booking Calendar</h3>
+                  <a href="https://eonasdan.github.io/bootstrap-datetimepicker/">How to Calendar</a>
+              
               </div>
 
               
             </div>
           </div>
+
+          <div class="row">
+            <div class="col-12">
+              <center>
+              <button type="button" class="btn btn-outline-dark">Delete</button>
+              <button type="button" class="btn btn-outline-dark">Edit</button>
+              <button type="button" class="btn btn-outline-dark">Book</button></center>
+            </div>
+          </div>
+
         </div>
       </div>
     )
