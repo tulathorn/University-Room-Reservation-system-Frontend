5e01a4acdbb08af39f67a80e5cac5fcb2b3cf908
tulathorn <tul.tulathorn@gmail.com>
2019-02-15 12:46:22 +0700

    commit

--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -29,7 +29,7 @@ class ReservationForm extends React.Component {
   }
 
   componentDidMount() {
-    console.log('Room Data =', this.props)
+    console.log('RoomData =', this.props)
   }
 
   search = async () => {
@@ -42,145 +42,130 @@ class ReservationForm extends React.Component {
     return (
       <div>
         <div className="jumbotron" style={jumbotronStyle}>
-          <Heading>Find a Room</Heading>
-
+          <Heading> Find a Room </Heading>
           <hr className="my-4" color="white" />
-          <SubHeading>Info :</SubHeading>
+          <SubHeading> Info: </SubHeading>{' '}
           <form>
             <div className="form-row">
               <div className="form-group col-md-4">
                 <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
-                  Date
-                </label>
+                  Date{' '}
+                </label>{' '}
                 <input
                   type="date"
                   className="form-control my-1 mr-sm-2"
                   id="inlineFormCustomSelectPref"
                   onChange={this.setForm('date')}
-                />
-              </div>
+                />{' '}
+              </div>{' '}
               <div className="form-group col-md-4">
                 <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
-                  Building
-                </label>
+                  Building{' '}
+                </label>{' '}
                 <select
                   className="custom-select my-1 mr-sm-2"
                   id="inlineFormCustomSelectPref"
                   onChange={this.setForm('Building')}
                 >
-                  <option selected>Building Name</option>
-                  <option value="Witsawa Watthana">Witsawa Watthana</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-              </div>
+                  <option selected> Building Name </option>{' '}
+                  <option value="Witsawa Watthana"> Witsawa Watthana </option>{' '}
+                  <option value="2"> Two </option> <option value="3"> Three </option>{' '}
+                </select>{' '}
+              </div>{' '}
               <div className="form-group col-md-4">
                 <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
-                  Size
-                </label>
+                  Size{' '}
+                </label>{' '}
                 <input
                   type="text"
                   className="form-control my-1 mr-sm-2"
                   id="inlineFormCustomSelectPref"
                   onChange={this.setForm('PeopleCapacity')}
-                />
-              </div>
-            </div>
+                />{' '}
+              </div>{' '}
+            </div>{' '}
           </form>
-
           <hr className="my-4" color="white" />
-          <SubHeading>Time :</SubHeading>
+          <SubHeading> Time: </SubHeading>{' '}
           <form className="form-inline">
             <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
-              From
-            </label>
+              From{' '}
+            </label>{' '}
             <select className="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-              <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+              <option selected> Choose... </option> <option value="1"> One </option>{' '}
+              <option value="2"> Two </option> <option value="3"> Three </option>{' '}
+            </select>{' '}
             <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
               :
-            </label>
+            </label>{' '}
             <select className="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-              <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+              <option selected> Choose... </option> <option value="1"> One </option>{' '}
+              <option value="2"> Two </option> <option value="3"> Three </option>{' '}
+            </select>{' '}
             <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
-              To
-            </label>
+              To{' '}
+            </label>{' '}
             <select className="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-              <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+              <option selected> Choose... </option> <option value="1"> One </option>{' '}
+              <option value="2"> Two </option> <option value="3"> Three </option>{' '}
+            </select>{' '}
             <label className="my-1 mr-2" for="inlineFormCustomSelectPref" style={textColor}>
               :
-            </label>
+            </label>{' '}
             <select className="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-              <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
+              <option selected> Choose... </option> <option value="1"> One </option>{' '}
+              <option value="2"> Two </option> <option value="3"> Three </option>{' '}
+            </select>{' '}
           </form>
-
           <hr className="my-4" color="white" />
-          <SubHeading>Filters :</SubHeading>
+          <SubHeading> Filters: </SubHeading>{' '}
           <div className="form-row">
             <div className="col-sm-4">
               <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                 <label className="form-check-label" for="defaultCheck1" style={textColor}>
-                  Teacher Computer
-                </label>
+                  Teacher Computer{' '}
+                </label>{' '}
                 <br />
                 <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                 <label className="form-check-label" for="defaultCheck1" style={textColor}>
-                  Student Computer
-                </label>
-              </div>
+                  Student Computer{' '}
+                </label>{' '}
+              </div>{' '}
             </div>
-
             <div className="col-sm-4">
               <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                 <label className="form-check-label" for="defaultCheck1" style={textColor}>
-                  Projector
-                </label>
+                  Projector{' '}
+                </label>{' '}
                 <br />
                 <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                 <label className="form-check-label" for="defaultCheck1" style={textColor}>
-                  White Board
-                </label>
-              </div>
+                  White Board{' '}
+                </label>{' '}
+              </div>{' '}
             </div>
-
             <div className="col-sm-4">
               <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                 <label className="form-check-label" for="defaultCheck1" style={textColor}>
-                  Air Conditioner
-                </label>
+                  Air Conditioner{' '}
+                </label>{' '}
                 <br />
                 <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                 <label className="form-check-label" for="defaultCheck1" style={textColor}>
-                  Visualizer
-                </label>
-              </div>
-            </div>
-          </div>
+                  Visualizer{' '}
+                </label>{' '}
+              </div>{' '}
+            </div>{' '}
+          </div>{' '}
         </div>
-
         <center>
           <button type="button" className="btn btn-outline-dark" onClick={this.search}>
-            Search
-          </button>
-        </center>
+            Search{' '}
+          </button>{' '}
+        </center>{' '}
       </div>
     )
   }
